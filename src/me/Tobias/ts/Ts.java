0ace812a25f406e0126b592c80bde77b50a104be
tobiasthe <tobias-m99@gmx.de>
2014-07-17 19:10:50 +0200

    V1.4

--- src/me/Tobias/ts/Ts.java
+++ src/me/Tobias/ts/Ts.java
@@ -2,18 +2,29 @@ package me.Tobias.ts;
 
 import java.util.logging.Logger;
 
+import me.Tobias.ts.Updater.ReleaseType;
 import me.Tobias.ts.Updater.UpdateResult;
 import me.Tobias.ts.commands.*;
 
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-public class Ts extends JavaPlugin
+@SuppressWarnings("unused")
+public class Ts extends JavaPlugin implements CommandExecutor
 {
+	
+	
+	
+	public static boolean update = false;
+	public static String name = "";
+	public static ReleaseType type = null;
+	public static String version = "";
+	public static String link = "";
+	public static String link2 = "http://dev.bukkit.org/bukkit-plugins/teamspeak-ip/";
 	public String prefix = "[TeamSpeakIP] ";
     public static Ts instance;
 	Logger log = Bukkit.getLogger();
@@ -24,43 +35,47 @@ public class Ts extends JavaPlugin
     }
     
     public void onEnable()
-    {
-    	if(this.getConfig().getBoolean("options.updater")){
-    	  Updater updater = new Updater(this, 70774, this.getFile(), Updater.UpdateType.NO_DOWNLOAD, false); // Start Updater but just do a version check
-    	  if(updater.getResult() == UpdateResult.UPDATE_AVAILABLE){
-    		  log.info(prefix+"New version available! "+ updater.getLatestName());
-    	  }
-    	 
-    	  
-    	  
-    	}
-    	/* Metrics */
-    	if(this.getConfig().getBoolean("options.Metrics")){
-    	try {
-            
-            Metrics metrics = new Metrics(this);
-            metrics.start();
-        } catch(Exception ex) {
-        	// Failed to submit the stats :-(
-        }
-    	}
-        
-    	 log.info(prefix+"Plugin enabeld ");
-    	 
-    	 instance = this;
-    	 this.getCommand("ts").setExecutor(new ts());
-    	 PluginManager pm = this.getServer().getPluginManager();
-    	 pm.registerEvents(new join(), this);
-    	 this.getConfig().options().header("Plugin by Tobias_the_best_!                 "+"                          Change at your own risk");
-    	 this.getConfig().addDefault("messages.reload", "§3Reload Erfolgreich!");
-    	 this.getConfig().addDefault("messages.ts3", "§2Die TS3 IP von <ServerName> lautet: §1TobiasTheBest.de");
-    	 this.getConfig().addDefault("messages.konsole", "Dieser Befehl kann nur von einen Spieler ausgefürt werden!");
-    	 this.getConfig().set("options.effects", true);
-    	 this.getConfig().set("options.sounds", true);
-    	 this.getConfig().set("options.Metrics", true);
-    	 this.getConfig().set("options.updater", true);
-    	 this.getConfig().options().copyDefaults(true);
-    	 this.saveConfig();
+    { 	
+        on();
+        log.info(prefix+"Plugin enabeld "); 	 
     }
-   
+        
+    public void on(){
+       	 instance = this;
+      	 this.getCommand("ts").setExecutor(new ts());
+      	 this.getCommand("tsupdate").setExecutor(new tsupdate());
+      	 PluginManager pm = this.getServer().getPluginManager();
+      	 pm.registerEvents(new join(), this);
+      	 this.getConfig().options().header("Plugin by Tobias_the_best_!                 "+"                          Change at your own risk");
+      	 this.getConfig().addDefault("messages.reload", "§3Reload Erfolgreich!");
+      	 this.getConfig().addDefault("messages.ts3", "§3TeamSpeak3: §6TobiasTheBest.de");
+      	 this.getConfig().addDefault("messages.konsole", "Dieser Befehl kann nur von einen Spieler ausgefürt werden!");
+      	 this.getConfig().addDefault("messages.no-permission", "Du Hast keine Rechte dafür");
+      	 this.getConfig().set("options.effects", true);
+      	 this.getConfig().set("options.sounds", true);
+      	 this.getConfig().set("options.Metrics", true);
+      	 this.getConfig().set("options.update-check", true);
+      	 this.getConfig().set("options.updateinfo", true);
+      	 this.getConfig().options().copyDefaults(true);
+      	 this.saveConfig();
+    	Updater();
+    }
+             //updater
+             public void Updater(){
+        	 if(this.getConfig().getBoolean("options.update-check")){
+           	  Updater updater = new Updater(this, 70774, this.getFile(), Updater.UpdateType.NO_DOWNLOAD, false); // Start Updater but just do a version check
+           	  update = updater.getResult() == Updater.UpdateResult.UPDATE_AVAILABLE; // Determine if there is an update ready for us
+           	  name = updater.getLatestName(); // Get the latest name
+           	  version = updater.getLatestGameVersion(); // Get the latest game version
+           	  type = updater.getLatestType(); // Get the latest file's type
+           	  link = updater.getLatestFileLink(); // Get the latest link
+           	  if(update){
+           		  log.info(prefix + "New version available! " + name);
+           		  log.info(prefix + "Download at " + link2);  	
+           	  }
+           	}
+         }//update ende
+             public void froce(){
+            	 Updater updater = new Updater(this, 70774, this.getFile(), Updater.UpdateType.NO_VERSION_CHECK, true); // Go straight to downloading, and announce progress to console.
+             }
 }
--- src/me/Tobias/ts/commands/ts.java
+++ src/me/Tobias/ts/commands/ts.java
@@ -13,9 +13,11 @@ import org.bukkit.potion.PotionEffectType;
 
 import me.Tobias.ts.Ts;
 
-public class ts implements CommandExecutor
-{
+public class ts implements CommandExecutor{
+
 	
+	
+
 	public static Ts pl = Ts.instance;
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
@@ -29,27 +31,27 @@ public class ts implements CommandExecutor
 			{
 				if(p.hasPermission("ts.reload"))
 				{
-					pl.reloadConfig();
+				 pl.reloadConfig();
+				 p.sendMessage(ChatColor.YELLOW+"[]================"+ChatColor.GOLD +" TeamSpeak " +ChatColor.YELLOW+"===============[]");
+				 p.sendMessage("");
        		     p.sendMessage(pl.getConfig().getString("messages.reload"));
+       		     p.sendMessage("");
+       		     p.sendMessage(ChatColor.YELLOW+"[]================"+ChatColor.GOLD +" TeamSpeak " +ChatColor.YELLOW+"===============[]");
 				}else
 				{
 					p.sendMessage(pl.getConfig().getString("messages.no-permission"));
 				}
 			}
-			if(args[0].equalsIgnoreCase("update"))
-			{
-				if(p.hasPermission("ts.update"))
-				{
-					p.sendMessage(ChatColor.AQUA+"Kommt bald");
-					p.sendMessage(ChatColor.AQUA+"Schau Selber nach ;D");
-					p.sendMessage(ChatColor.BLUE+"http://dev.bukkit.org/bukkit-plugins/teamspeak-ip/");
-				}
-			}
-			  
-		}else
+			
+	
+            }else
         {
 		  World w = p.getWorld();
+		  p.sendMessage(ChatColor.YELLOW+"[]================"+ChatColor.GOLD +" TeamSpeak " +ChatColor.YELLOW+"===============[]");
+		  p.sendMessage("");
 		  p.sendMessage(pl.getConfig().getString("messages.ts3"));
+		  p.sendMessage("");
+		  p.sendMessage(ChatColor.YELLOW+"[]================"+ChatColor.GOLD +" TeamSpeak " +ChatColor.YELLOW+"===============[]");
 		  if(pl.getConfig().getBoolean("options.effects"))
 		  {
       	  p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 35, 100));
@@ -62,11 +64,14 @@ public class ts implements CommandExecutor
           w.playSound(p.getPlayer().getLocation(), Sound.NOTE_PIANO, 25,1);
       	  w.playSound(p.getPlayer().getLocation(), Sound.ARROW_HIT, 25,1);
       	  }		  
+          
           }
+          
 			return true;
 			} else
 			{
 				sender.sendMessage(pl.getConfig().getString("messages.konsole"));
+
 			}
 		return false;
 	}
