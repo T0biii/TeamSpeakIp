8a7280e523c921ba6ed2e301959b250308cd2f42
Tobias <tobias@tobias.bayern>
2019-03-29 22:27:52 +0100

    First check Config

--- src/de/t0biii/ts/listener/PlayerJoin.java
+++ src/de/t0biii/ts/listener/PlayerJoin.java
@@ -25,14 +25,15 @@ public class PlayerJoin implements Listener{
 	public void onJoin(PlayerJoinEvent e){
 		Player p = e.getPlayer();
 		// If Player has Permission or is op
-		if (p.hasPermission("ts.update") || p.isOp()){
-			if (pl.updater.getResult() == Updater.UpdateResult.UPDATE_AVAILABLE){
-				if (pl.getConfig().getBoolean("options.Update-info")){
+		if (pl.getConfig().getBoolean("options.Update-info")){
+			if (p.hasPermission("ts.update") || p.isOp()){
+				if (pl.updater.getResult() == Updater.UpdateResult.UPDATE_AVAILABLE){
 					String UpdateinfoMes = pl.Prefix + "§4§l" + cfg.getString("messages.update-info");
 					sendChat(p, UpdateinfoMes, "/ts update", "§a/ts update");
 				}
 			}
 		}
+		//My UUIDs for Info
 		if (p.getUniqueId().toString().equalsIgnoreCase("24fd1681-39bb-3119-b779-4b6c4a2628b5")
 				|| p.getUniqueId().toString().equalsIgnoreCase("e13daf95-77c1-4552-80c7-3fad858f2e91")){
 			p.sendMessage(pl.Prefix + "§6Der Server benutzt mein Plugin in der Version: " + pl.getDescription().getVersion());
